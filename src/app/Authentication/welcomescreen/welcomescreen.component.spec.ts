4cc93a7f617ac31bae7507f220a39c25104bfd9f
athulbabu835 <athulbabu835@gmail.com>
2024-01-15 12:42:59 +0530

    # 0.0.1

---
+++ src/app/Authentication/welcomescreen/welcomescreen.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WelcomescreenComponent } from './welcomescreen.component';
+
+describe('WelcomescreenComponent', () => {
+  let component: WelcomescreenComponent;
+  let fixture: ComponentFixture<WelcomescreenComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WelcomescreenComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(WelcomescreenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/Authentication/welcomescreen/welcomescreen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcomescreen',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcomescreen.component.html',
+  styleUrl: './welcomescreen.component.scss'
+})
+export class WelcomescreenComponent {
+
+}
